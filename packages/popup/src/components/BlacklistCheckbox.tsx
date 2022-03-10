cb076dcfe7a9cfffe428e6b0935f7f5aa7a93ea3
Blagoj <blagoj.petrov5@gmail.com>
2022-03-10 20:53:28 +0100

    fix indent

--- packages/popup/src/components/BlacklistCheckbox.tsx
+++ packages/popup/src/components/BlacklistCheckbox.tsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
   input[type='checkbox'] {
-		--active: hsl(142.7,46.05%,52.745%);
+    --active: hsl(142.7,46.05%,52.745%);
     --active-inner: #fff;
     --focus: 2px #27fe794c;
     --border: #BBC1E1;
-		--border-hover: hsl(142.7,46.05%,52.745%);
+    --border-hover: hsl(142.7,46.05%,52.745%);
     --background: #fff;
     --disabled: #F6F8FF;
     --disabled-inner: #E1E6F9;
@@ -27,7 +27,7 @@ const Container = styled.div`
     border: 1px solid var(--bc, var(--border));
     background: var(--b, var(--background));
     transition: background .3s, border-color .3s, box-shadow .2s;
-		border-radius: 7px;
+    border-radius: 7px;
 
     :after {
       content: '';
@@ -36,15 +36,15 @@ const Container = styled.div`
       top: 0;
       position: absolute;
       transition: transform var(--d-t, .3s) var(--d-t-e, ease), opacity var(--d-o, .2s);
-			opacity: var(--o, 0);
-			width: 7px;
-			height: 10px;
-			border: 2px solid var(--active-inner);
-			border-top: 0;
-			border-left: 0;
-			left: 7px;
-			top: 4px;
-			transform: rotate(var(--r, 20deg));
+      opacity: var(--o, 0);
+      width: 7px;
+      height: 10px;
+      border: 2px solid var(--active-inner);
+      border-top: 0;
+      border-left: 0;
+      left: 7px;
+      top: 4px;
+      transform: rotate(var(--r, 20deg));
     }
 
     :checked {
@@ -53,8 +53,8 @@ const Container = styled.div`
       --d-o: .3s;
       --d-t: .6s;
       --d-t-e: cubic-bezier(.2, .85, .32, 1.2);
-			--o: 1;
-			--r: 43deg;
+      --o: 1;
+      --r: 43deg;
     }
     :disabled {
       --b: var(--disabled);
@@ -82,21 +82,21 @@ const Container = styled.div`
 `;
 
 const Label = styled.label<{ isChecked: boolean }>`
-	font-size: 14px;
-	line-height: 21px;
-	display: inline-block;
-	vertical-align: top;
-	cursor: pointer;
-	margin-left: 4px;
-	color: var(--text-color);
-	margin-right: 0.9rem;
-	letter-spacing: 1px;
-	text-decoration: ${({ isChecked }) => (isChecked ? 'line-through' : 'unset')};
+  font-size: 14px;
+  line-height: 21px;
+  display: inline-block;
+  vertical-align: top;
+  cursor: pointer;
+  margin-left: 4px;
+  color: var(--text-color);
+  margin-right: 0.9rem;
+  letter-spacing: 1px;
+  text-decoration: ${({ isChecked }) => (isChecked ? 'line-through' : 'unset')};
 `;
 
 type Props = {
-	isChecked: (domain: string) => boolean | boolean;
-	onCheck: (checked: boolean, domain: string) => void;
+  isChecked: (domain: string) => boolean | boolean;
+  onCheck: (checked: boolean, domain: string) => void;
 }
 
 function Checkbox({ isChecked, onCheck }: Props) {
@@ -105,8 +105,8 @@ function Checkbox({ isChecked, onCheck }: Props) {
   useEffect(() => {
     const storageChangeListener = (
       changes: {
-			[key: string]: browser.storage.StorageChange;
-			},
+      [key: string]: browser.storage.StorageChange;
+      },
       areaName: string,
     ) => {
       if (areaName !== 'local') return;
