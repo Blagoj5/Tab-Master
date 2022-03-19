0d38877c975593be121ac062f0a7d49938750294
Blagoj <blagoj.petrov5@gmail.com>
2022-03-19 01:51:18 +0100

    Add eslint and prettier config  to root

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.jest.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+  ],
+  rules: {},
+};
