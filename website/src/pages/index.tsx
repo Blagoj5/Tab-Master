761eec5e041898bad8a6844ee87c879f9e2128ab
Blagoj <blagoj.petrov5@gmail.com>
2022-03-24 22:34:50 +0100

    Improve website

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
     setExtensionUrl(url);
   }, []);
   return (
-    <div className="bg-primary h-screen min-h-50 flex flex-col">
+    <div className="relative bg-primary h-screen min-h-50 flex flex-col">
       <header className="h-14 flex items-center justify-between">
         {/* logo */}
         <Link href="/" passHref>
@@ -62,8 +62,8 @@ const Home: NextPage = () => {
           ))}
         </div>
       </header>
-      <div className="flex flex-1 items-center justify-center">
-        <div className="flex flex-col max-w-2xl text-center space-y-8">
+      <div className="flex flex-1 flex-col-reverse xl:flex-row z-10 xl:space-x-10 items-center justify-center w-full px-6 xl:px-8">
+        <div className="flex flex-col max-w-2xl text-center xl:text-left space-y-8">
           <h1 className="text-gray-300 text-5xl mb-2 leading-tight">
             Easy and intuitive way to{' '}
             <span className="text-secondary">navigate</span> trough tabs and
@@ -75,7 +75,7 @@ const Home: NextPage = () => {
             providing an easy and intuitive way to navigate through tabs and
             history.
           </p>
-          <div className="space-x-4">
+          <div className="flex flex-col space-y-6 sm:space-x-4 sm:block">
             <a
               href={extensionUrl}
               target="_blank"
@@ -87,13 +87,16 @@ const Home: NextPage = () => {
             <a
               href="https://github.com/Blagoj5/Tab-Master"
               target="_blank"
-              className="inline-flex items-center text-gray-300 bg-black bg-opacity-25 py-4 px-14 rounded-md text-xl hover:bg-opacity-20"
+              className="inline-flex items-center justify-center text-gray-300 bg-black bg-opacity-25 py-4 px-14 rounded-md text-xl hover:bg-opacity-20"
               rel="noreferrer"
             >
               <GitIcon />
               <span className="ml-2">Github</span>
             </a>
           </div>
+        </div>
+        <div className="aspect-video relative opacity-60 w-full sm:w-10/12 max-w-[750px] mb-12 xl:mb-0 xl:min-w-[730px] xl:max-w-[800px]">
+          <Image src="/tab-master.gif" alt="No img" layout="fill" />
         </div>
       </div>
     </div>
