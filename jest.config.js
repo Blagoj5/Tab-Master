2324db8fbe52bc291078a9dfe4df4a60686be933
Blagoj <blagoj.petrov5@gmail.com>
2022-03-27 16:50:48 +0200

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -147,17 +147,12 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
+    '**/__tests__/**/*.spec.[jt]s?(x)',
     // "**/?(*.)+(spec|test).[tj]s?(x)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/__tests__/.eslintrc.js',
-    '/__tests__/utils',
-    '/__tests__/data',
-  ],
+  testPathIgnorePatterns: ['/node_modules/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
